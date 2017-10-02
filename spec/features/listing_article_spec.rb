f2247cd10381ad8ccd49055d6c6cabc845c2a22c
denialtorres <denial.torres@gmail.com>
2017-10-02 17:31:34 +0000

    Implement displaying a message when no articles present

---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper" 
+
+RSpec.feature "Listing Articles" do
+  before do
+    @article1 = Article.create(title: "The First article",
+                              body: "Lorem ipsum dolor sit amet, consectetur")
+    @article2 = Article.create(title: "The second Article",
+                              body: "Pellentesque ac ligula in tellus fegiat.")
+  end
+  
+  scenario "A user lists all articles" do
+    visit "/"
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+  
+  scenario "A user has no articles" do
+    Article.delete_all
+    visit "/"
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+    
+    within("h1#no-articles") do 
+      expect(page).to have_content("No Articles Created")
+    end
+  end
+    
+end
