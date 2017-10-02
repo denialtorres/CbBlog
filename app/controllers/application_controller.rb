36328b1106fc32d04edbdc44d1ecf551ee17807d
denialtorres <denial.torres@gmail.com>
2017-10-02 18:27:58 +0000

    Handle article not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :resourse_not_found
+  
+  protected
+  
+  def resourse_not_found
+    
+  end
 end
